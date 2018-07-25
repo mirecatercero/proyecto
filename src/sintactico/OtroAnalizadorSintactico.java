f5205e5269d6e1b8a1046796c15c5ab90a927fc2
José Antonio Medina Quintana <medina.octubre95@live.com.mx>
2018-07-25 10:08:19 -0500

    Ya pasamos :3

--- src/sintactico/OtroAnalizadorSintactico.java
+++ src/sintactico/OtroAnalizadorSintactico.java
@@ -5,6 +5,7 @@
  */
 package sintactico;
 
+import java.util.ArrayList;
 import java.util.Stack;
 import javax.swing.JTextArea;
 
@@ -13,62 +14,90 @@ import javax.swing.JTextArea;
  * @author Jose
  */
 public class OtroAnalizadorSintactico {
-    SyntacticAnlysisTable syntacticAnlysisTable = new SyntacticAnlysisTable();
-    Lexema lexema;
-    String terminal, regla, segundaRegla, s = "", error = "";
-    Stack<String> reglas = new Stack<String>();
-    int columna;
-    String[] reglaSeparada, segundaReglaSeparada;
     
-    public void analisisSintactico(Stack<Lexema> pila, JTextArea txtTraza)
-    {
-        reglas.push("PRINCIPAL");
-        while(!pila.empty())
+    //public static ArrayList<String> Codigo = AnalisisLexico.Codigo;
+    public static ArrayList<String> tabla = new ArrayList<>();
+    SyntacticAnlysisTable sintactica = new SyntacticAnlysisTable();
+    Lexema lexema;
+    String simbolo;
+    String error;
+          
+    public void analisis(ArrayList<Lexema> Codigo, JTextArea txtTraza){
+        error = "";
+        int ban =0; 
+        Codigo.add(new Lexema(100, "$", 0));
+        tabla.add("$");
+        tabla.add("Inicio");
+        String rempleaso;
+        
+        while (!Codigo.isEmpty())
         {
-            lexema = pila.peek();
-            
-            if(lexema.getID() == 37 || lexema.getID() == 38 || lexema.getID() == 39)
-                terminal = "val";
+            lexema = Codigo.get(0);
+            if(lexema.getID() == 37)
+                simbolo = "INT";
+            else if(lexema.getID() == 38)
+                simbolo = "FLOAT";
+            else if(lexema.getID() == 39)
+                simbolo = "STRING";
             else if(lexema.getID() == 0)
-                terminal = "id";
-            else terminal = lexema.getToken();
+                simbolo = "id";
+            else if(lexema.getID() == 8 || lexema.getID() == 9)
+                simbolo = "BOOLEAN";
+            else simbolo = lexema.getToken();
             
-            if(terminal.equals(reglas.peek()))
+            if(simbolo.equals(tabla.get((tabla.size()-1))))
             {
-                if(!pila.empty())
-                    pila.pop();
-                if(!reglas.empty())
-                    reglas.pop();
+                Codigo.remove(0);
+                tabla.remove((tabla.size()-1));
                 continue;
             }
             
-            if(!reglas.empty())
-                s = reglas.pop();
+            lexema = Codigo.get(0);
+            if(lexema.getID() == 37)
+                simbolo = "INT";
+            else if(lexema.getID() == 38)
+                simbolo = "FLOAT";
+            else if(lexema.getID() == 39)
+                simbolo = "STRING";
+            else if(lexema.getID() == 0)
+                simbolo = "id";
+            else if(lexema.getID() == 8 || lexema.getID() == 9)
+                simbolo = "BOOLEAN";
+            else simbolo = lexema.getToken();
             
-            if(!pila.empty())
-            {
-                lexema = pila.peek();
-                txtTraza.setText(txtTraza.getText() + "\n" + "Analizando el token " + lexema.getToken() + "\n");
-                regla = syntacticAnlysisTable.buscaReglas(lexema, s);
-            }
+            txtTraza.setText(txtTraza.getText() + "\n" + "Analizando el token " + lexema.getToken() + "\n");
             
-            if(!regla.equals("ERROR"))
+            rempleaso = sintactica.buscaReglas(simbolo, tabla.remove((tabla.size()-1)));
+            if(!rempleaso.equals(""))
             {
-                txtTraza.setText(txtTraza.getText() + "Regla a analizar: " + regla + "\n");
-                System.out.println("Analizando el pedo de : " + terminal + " " + s);
-                if(!regla.equals("E"))
+                txtTraza.setText(txtTraza.getText() + "Regla a analizar: " + sintactica.tabla2[sintactica.not][sintactica.ter] + "\n");
+                if(!rempleaso.equals("E"))
                 {
-                    reglaSeparada = regla.split(" -> ");
-                    reglaSeparada = reglaSeparada[1].split(" ");
-                        for(int i = reglaSeparada.length - 1; i >= 0; i--)
-                        {
-                            if(!reglaSeparada[i].equals("E"))
-                                reglas.push(reglaSeparada[i]);
-                        }
+                    String[] separa = rempleaso.split(" ");
+                    for (int i = separa.length-1; i >= 0 ; i--)
+                        tabla.add(separa[i]);
                 }
             }
-            else error += "Error de sintaxis en linea " + lexema.getLinea() + ", cerca de " + lexema.getToken() + "\n";
+            else
+            {
+                error += "Error de sintaxis en linea " + lexema.linea + ", cerca de " + lexema.getToken();
+                System.out.println("Error alv");
+                tabla.clear();
+                Codigo.clear();
+                ban = 1;
+                break;
+            }     
         }
+        
+        if(tabla.isEmpty() && Codigo.isEmpty() && ban ==0)
+            System.out.println("Sobres simon todo chido");
+        else
+        {
+            if(error.equals(""))
+                error += "Error de sintaxis en linea " + lexema.linea + ", cerca de " + lexema.getToken();
+            tabla.clear();
+            Codigo.clear();
+        }   
     }
     
     public String getErrors()
